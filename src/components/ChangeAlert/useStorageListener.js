c5236b1197d28dbff800bdd227d8ad2a2e6c61c5
klich84 <klich84@gmail.com>
2023-02-07 14:57:19 -0500

    fix(App): :gear: delete console.log

--- src/components/ChangeAlert/useStorageListener.js
+++ src/components/ChangeAlert/useStorageListener.js
@@ -5,7 +5,6 @@ function useStorageListener(sincronize) {
   
   window.addEventListener('storage', (change) => {
     if (change.key === 'TODOS_V1') {
-      console.log('Hubo cambios en TODOS_V1');
       setStorageChange(true);
     }
   });
